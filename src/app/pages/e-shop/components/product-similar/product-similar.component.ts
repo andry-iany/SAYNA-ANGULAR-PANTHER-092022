0f81e72f6e0f1bf502f2f19b7fecded666a98fe7
Andry <andrysitrakiniainasoa@gmail.com>
2022-09-20 15:56:37 +0300

    edit styles in similar product components

--- src/app/pages/e-shop/components/product-similar/product-similar.component.ts
+++ src/app/pages/e-shop/components/product-similar/product-similar.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { map } from 'rxjs';
+import { TArticle } from '../../e-shop.model';
 import { ProductService } from '../../services/product.service';
+
+type Maybe<T> = T | null;
 
 @Component({
   selector: 'app-product-similar',
@@ -8,8 +11,25 @@ import { ProductService } from '../../services/product.service';
   styleUrls: ['./product-similar.component.scss'],
 })
 export class ProductSimilarComponent implements OnInit {
+  @Input() article: Maybe<TArticle> = null;
+
   constructor(private productService: ProductService) {}
-  recommendations$ = this.productService.getAllArticles({ _limit: 3 });
+
+  recommendations$ = this.productService.getAllArticles().pipe(
+    map((articles) => {
+      return articles
+        .filter((article) =>
+          this.areArticlesSameCategory(article, this.article)
+        )
+        .slice(0, 3);
+    })
+  );
 
   ngOnInit(): void {}
+
+  private areArticlesSameCategory(art1: TArticle, art2: Maybe<TArticle>) {
+    return (
+      art1.category.trim() === art2?.category.trim() && art1.id !== art2.id
+    );
+  }
 }
