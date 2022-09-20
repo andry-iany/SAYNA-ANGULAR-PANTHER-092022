1d0c02cb2b9019c48e50d0e40c6da1f12981910c
Andry <andrysitrakiniainasoa@gmail.com>
2022-09-20 19:19:43 +0300

    add badge to basket-img

--- src/app/shared/basket-img/basket-img.component.ts
+++ src/app/shared/basket-img/basket-img.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/pages/e-shop/services/cart.service';
 
 @Component({
   selector: 'app-basket-img',
   templateUrl: './basket-img.component.html',
-  styleUrls: ['./basket-img.component.scss']
+  styleUrls: ['./basket-img.component.scss'],
 })
 export class BasketImgComponent implements OnInit {
+  constructor(public cartService: CartService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
