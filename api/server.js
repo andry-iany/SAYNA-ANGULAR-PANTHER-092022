dcf9462b4a569e8e4c16566c0ebca885d91b1630
Andry <andrysitrakiniainasoa@gmail.com>
2022-09-24 22:58:18 +0300

    update json-server implementation

--- api/server.js
+++ api/server.js
@@ -1,12 +1,19 @@
-const jsonServer = require("./json-server");
+const resolve = require("path").resolve;
+
+require("dotenv").config({ path: resolve(__dirname, ".env") });
+
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5678;
+
+const middlewares = jsonServer.defaults({
+  static: resolve(__dirname, "public"),
+});
+
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-
 server.listen(port, () => {
-  console.log(`Server listening on port: ${port}!`);
+  console.log(`JSON Server is running on port ${port}`);
 });
